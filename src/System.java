f36f737829f49d2ec24e106d7dc4b6849a206d20
lavatahir <lavatahir@2201-012.ODENG2.CARLETON.CA>
2018-03-01 15:39:02 -0500

    Adding the initial files

---
+++ src/System.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+
+public class System {
+	
+	private Server server;
+	private ArrayList<Event> eventList;
+	private double clock;
+
+	public System() {
+		this.server = new Server();
+		this.eventList = new ArrayList<Event>();
+		this.clock = 0;
+	}
+
+	 /* methods to be added here */
+
+}
